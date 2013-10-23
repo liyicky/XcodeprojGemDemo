e6c2348ab258b60823f5a22bcbe557871f4462c6
liyicky <liyicky@gmail.com>
2013-10-23 13:58:51 -0400

    tests profile_action should change a scheme

--- spec/ao_scheme_spec.rb
+++ spec/ao_scheme_spec.rb
@@ -10,6 +10,11 @@ describe "AO_scheme" do
       @project      = XcodeTestProj.new("TestingProject_wY0kK5cNi8", "TestingProject")
       @project_path = @project.project_path
       @root_path    = @project.root_path
+
+      @scheme       = Xcodeproj::XCScheme.new
+      @main_target  = @project.project.new_target(:application, 'Main', :ios)
+      @test_target  = @project.project.new_target(:application, 'Test', :ios)
+      @project.saveProject
     end
 
     after (:each) do 
@@ -17,23 +22,13 @@ describe "AO_scheme" do
       @project      = nil
       @project_path = nil
       @root_path    = nil
+      @scheme       = nil
+      @main_target  = nil
+      @test_target  = nil
       FileUtils.rm_rf("TestingProject_wY0kK5cNi8")
     end
 
     describe "test_action" do
-
-      before (:each) do
-        @scheme = Xcodeproj::XCScheme.new
-        @main_target = @project.project.new_target(:application, 'Main', :ios)
-        @test_target = @project.project.new_target(:application, 'Test', :ios)
-        @project.saveProject
-      end
-
-      after (:each) do
-        @scheme      = nil
-        @main_target = nil
-        @test_target = nil
-      end
 
       it "Builds a test_action in a scheme" do
         @result = Xcodeproj::XCScheme.new
@@ -50,6 +45,17 @@ describe "AO_scheme" do
 
         @result = true if @scheme.to_s().include? "buildConfiguration = \"Main\">"
         @result.should == true
+      end
+    end
+
+    describe "profile_action" do
+
+      it "Builds a post_action in a scheme" do
+        @result = Xcodeproj::XCScheme.new
+        build = AO_scheme.new(@result, @main_target, @test_target, :debug)
+        build.profile_action
+
+        @result.should_not == @scheme
       end
     end
 
