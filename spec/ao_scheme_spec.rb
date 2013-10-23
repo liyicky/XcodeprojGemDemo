cbb932a8b0ad2eebdbe9c9f36c0588f876ac0e01
liyicky <liyicky@gmail.com>
2013-10-23 14:54:14 -0400

    Tests for ao_scheme profile_action, test_action

--- spec/ao_scheme_spec.rb
+++ spec/ao_scheme_spec.rb
@@ -30,33 +30,75 @@ describe "AO_scheme" do
 
     describe "test_action" do
 
-      it "Builds a test_action in a scheme" do
-        @result = Xcodeproj::XCScheme.new
-        build = AO_scheme.new(@result, @main_target, @test_target, :debug)
-        build.test_action("echo script", "Main")
+      before (:each) do
+        @result       = Xcodeproj::XCScheme.new
+        @scheme_build = AO_scheme.new(@result, @main_target, @test_target, :debug)
+      end 
 
+      after (:each) do
+        @scheme_build = nil
+      end 
+
+      it "Builds a test_action in a scheme" do
+        @scheme_build.test_action("echo script", "Main")
         @result.should_not == @scheme
       end
 
       it "Sets the buildConfiguration" do
-        build = AO_scheme.new(@scheme, @main_target, @test_target, :debug)
-        build.test_action("echo script", "Main")
-        build.save(@project_path,  "MainScheme")
+        @scheme_build.test_action("echo script", "Main")
 
-        @result = true if @scheme.to_s().include? "buildConfiguration = \"Main\">"
+        @result = true if @result.to_s().include? "buildConfiguration = \"Main\">"
+        @scheme = (true if @scheme.to_s().include? "buildConfiguration = \"Main\">") || false
         @result.should == true
+        @scheme.should == false
+      end
+
+      it "Adds Testable elements" do
+        @scheme_build.test_action("echo script", "Main")
+
+        @result = true if @result.to_s().include? "TestableReference"
+        @scheme = (true if @scheme.to_s().include? "TestableReference") || false
+        @result.should == true
+        @scheme.should == false
+      end
+
+      it "Adds script to Testable elements" do
+        @scheme_build.test_action("this is the script", "Main")
+        @result = @result.doc.root.elements["TestAction"]
+  
+        @result.to_s().should include "this is the script"
+        @scheme.to_s().should_not include "this is the script"
       end
     end
 
     describe "profile_action" do
 
+      before (:each) do
+        @result       = Xcodeproj::XCScheme.new
+        @scheme_build = AO_scheme.new(@result, @main_target, @test_target, :debug)
+      end 
+
+      after (:each) do
+        @scheme_build = nil
+      end 
+
+
       it "Builds a post_action in a scheme" do
-        @result = Xcodeproj::XCScheme.new
-        build = AO_scheme.new(@result, @main_target, @test_target, :debug)
-        build.profile_action
+        @scheme_build.profile_action
 
         @result.should_not == @scheme
       end
+
+      it "Adds BuildableProductRunnable elements" do
+        @scheme_build.profile_action
+        @result = @result.doc.root.elements["ProfileAction"]
+        @result = @result.elements["BuildableProductRunnable"]
+
+        @result.to_s().should include "BuildableProductRunnable"
+        @scheme.to_s().should_not include "BuildableProductRunnable"
+      end
+
+      
     end
 
 end
